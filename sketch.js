498c3c980c37459d9e78c391e601208a9ea673a2
Thalys001 <thalyssouza708@gmail.com>
2023-03-17 20:38:08 -0300

    feat: added car move function

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 let imagemDaEstrada;
 let imagemDoAtor;
 let imagemCarro;
+//carro
+let xCarro = 600;
 
 function preload() {
-  imagemDaEstrada = loadImage("/assets/images/estrada.png");
-  imagemDoAtor = loadImage("/assets/images/ator-1.png")
-  imagemCarro = loadImage("/assets/images/carro-1.png")
+  imagemDaEstrada = loadImage("/src/assets/images/estrada.png");
+  imagemDoAtor = loadImage("/src/assets/images/ator-1.png")
+  imagemCarro = loadImage("/src/assets/images/carro-1.png")
 }
 
 function setup() {
@@ -14,6 +16,19 @@ function setup() {
 
 function draw() {
   background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+}
+
+function mostraAtor(){
   image(imagemDoAtor, 100, 366, 30, 30)
-  image(imagemCarro, 400, 40, 50, 40)
+}
+
+function mostraCarro() {
+  image(imagemCarro, xCarro, 40, 50, 40)
+}
+
+function movimentaCarro(){
+  xCarro = xCarro - 2;
 }
