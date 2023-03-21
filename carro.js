09a1e2c33612f21461aec5577384792b3d262a6f
Thalys001 <thalyssouza708@gmail.com>
2023-03-20 23:46:36 -0300

    feat: created list of car positions

--- carro.js
+++ carro.js
@@ -1,38 +1,27 @@
-// carro 1
-let xCarro = 600;
-let yCarro = 40;
-let velocidadeCarro1 = 2;
-
-// carro 2
-let xCarro2 = 600;
-let yCarro2 = 96;
-let velocidadeCarro2 = 2.5;
-
-// carro 3
-let xCarro3 = 600;
-let yCarro3 = 150;
-let velocidadeCarro3 = 3.2;
+let xCarros = [600, 600, 600]
+let yCarros = [40, 96, 150]
+let velocidadeCarros = [2, 2.5, 3.2]
 
 function mostraCarro() {
-  image(imagemCarro, xCarro, yCarro, 50, 40);
-  image(imagemCarro2, xCarro2, yCarro2, 50, 40);
-  image(imagemCarro3, xCarro3, yCarro3, 50, 40);
+  image(imagemCarro, xCarros[0], yCarros[0], 50, 40);
+  image(imagemCarro2, xCarros[1], yCarros[1], 50, 40);
+  image(imagemCarro3, xCarros[2], yCarros[2], 50, 40);
 }
 
 function movimentaCarro() {
-  xCarro -= velocidadeCarro1;
-  xCarro2 -= velocidadeCarro2;
-  xCarro3 -= velocidadeCarro3;
+  xCarros[0] -= velocidadeCarros[0];
+  xCarros[1] -= velocidadeCarros[1];
+  xCarros[2] -= velocidadeCarros[2];
 }
 
 function voltaPosicaoInicial(){
-  if (xCarro < - 50){
-    xCarro = 600
+  if (xCarros[0] < - 50){
+    xCarros[0] = 600
   }
-  if (xCarro2 < - 50){
-    xCarro2 = 600
+  if (xCarros[1] < - 50){
+    xCarros[1] = 600
   }
-  if (xCarro3 < - 50){
-    xCarro3 = 600
+  if (xCarros[2] < - 50){
+    xCarros[2] = 600
   }
 }
